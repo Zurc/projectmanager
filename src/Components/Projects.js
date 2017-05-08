349aee3898a12b40638db5f157beaabd87642cb6
Zurc <cruzjurado@gmail.com>
2017-05-08 08:39:51 +0100

    create comp with state

---
+++ src/Components/Projects.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Projects extends Component {
+
+  render() {
+      console.log(this.props)
+    return (
+      <div className="Projects">
+        My projects <br/>
+             
+      </div>
+    );
+  }
+}
+
+export default Projects;
